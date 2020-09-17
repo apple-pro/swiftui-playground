6f62ba9211f3662719fe12aae76926eca15f5433
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-17 15:46:05 +0800

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by StartupBuilder.INFO on 9/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
