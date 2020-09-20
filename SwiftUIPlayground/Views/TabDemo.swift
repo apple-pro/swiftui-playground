49ae46d006dfe313329145ccb1a836058f7b4362
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-20 09:58:46 +0800

    Add code to manually switch tabs

--- SwiftUIPlayground/Views/TabDemo.swift
+++ SwiftUIPlayground/Views/TabDemo.swift
@@ -9,18 +9,36 @@ import SwiftUI
 
 
 struct TabDemo: View {
+    
+    @State private var activeTab = 2
+    
     var body: some View {
-        TabView {
-            Text("View 1").tabItem {
-                Image(systemName: "star")
-                Text("View 1")
-            }
+        
+        TabView(selection: $activeTab) {
+            Text("View 1")
+                .onTapGesture(count: 2) {
+                    switchTabs()
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("View 1")
+                }
+                .tag(1)
             
-            Text("View 2").tabItem {
-                Image(systemName: "star.fill")
-                Text("View 2")
-            }
+            Text("View 2")
+                .onTapGesture(count: 2) {
+                    switchTabs()
+                }
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("View 2")
+                }
+                .tag(2)
         }
+    }
+    
+    func switchTabs() {
+        activeTab = activeTab == 2 ? 1 : 2
     }
 }
 
