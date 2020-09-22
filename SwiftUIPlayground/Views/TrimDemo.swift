a8350d9f271860991971b8fabef3e3c48636850a
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-22 19:16:18 +0800

    Cool tricks with trim

--- SwiftUIPlayground/Views/TrimDemo.swift
+++ SwiftUIPlayground/Views/TrimDemo.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct TrimDemo: View {
+    
     @State private var completionAmount: CGFloat = 0.0
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
     
     
     var body: some View {
@@ -20,17 +21,27 @@ struct TrimDemo: View {
             
             Spacer()
             
-            Circle()
-                .trim(from: 0, to: completionAmount)
-                .stroke(Color.red, lineWidth: 20)
-                .frame(width: 200, height: 200)
-                .rotationEffect(.degrees(-90))
-                .onReceive(timer) { _ in
-                    withAnimation {
-                        guard self.completionAmount < 1 else { return }
-                        self.completionAmount += 0.2
+            ZStack {
+                Circle()
+                    .trim(from: 0, to: completionAmount)
+                    .stroke(completionAmount < 0.5 ? Color.red : Color.green, lineWidth: 40)
+                    .frame(width: 180, height: 180)
+                    .rotationEffect(.degrees(-90))
+                    .onReceive(timer) { _ in
+                        withAnimation {
+                            guard self.completionAmount < 1 else { return }
+                            self.completionAmount += 0.2
+                        }
                     }
-                }
+                    .background(Circle().frame(width: 240, height: 240))
+                
+                Text("\(Int(completionAmount*100))%")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+            }
+            
+            Spacer()
             
         }.padding()
         
