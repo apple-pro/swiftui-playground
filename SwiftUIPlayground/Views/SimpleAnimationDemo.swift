5658ca50bba117c6cf9dd77acd0958696d84da80
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-24 16:36:18 +0800

    Some controls for animation

--- SwiftUIPlayground/Views/SimpleAnimationDemo.swift
+++ SwiftUIPlayground/Views/SimpleAnimationDemo.swift
@@ -18,6 +18,12 @@ struct SimpleAnimationDemo: View {
     
     @State var animationType: Animation = Animation.default
     @State var animationAmount: CGFloat = 1
+    @State var delay: Double = 1
+    @State var damping: Double = 0.5
+    @State var stiffness: Double = 0.5
+    @State var duration: Double = 5
+    @State var mass: Double = 0.5
+    @State var initialVelocity: Double = 0.5
     
     var body: some View {
         VStack {
@@ -38,14 +44,36 @@ struct SimpleAnimationDemo: View {
                 
             }.frame(maxWidth: .infinity, maxHeight: .infinity)
             
-            Picker("Gender Identity", selection: $animationType) {
+            Form {
+                Section(header: Text("Spring")) {
+                    Stepper("Mass (\(mass))", value: $mass, in: 0...1, step: 0.1)
+                    Stepper("Stiffness (\(stiffness))", value: $stiffness, in: 0...1, step: 1)
+                    Stepper("Damping (\(damping))", value: $damping, in: 0...1, step: 0.5)
+                    Stepper("Initial Velocity (\(initialVelocity))", value: $initialVelocity, in: 0...1, step: 0.1)
+                }
+                
+                Section(header: Text("Ease In/Out")) {
+                    Stepper("Delay (\(delay))", value: $delay, in: 0...3, step: 0.5)
+                    Stepper("Duration (\(duration))", value: $duration, in: 0...3, step: 0.5)
+                }
+            }
+            
+            Picker("Animation Type", selection: $animationType) {
                 Text("Default").tag(Animation.default)
-                Text("Spring").tag(Animation.interpolatingSpring(stiffness: 50, damping: 1))
+                
+                Text("Spring").tag(Animation.interpolatingSpring(
+                                    mass: mass,
+                                    stiffness: stiffness,
+                                    damping: damping,
+                                    initialVelocity: initialVelocity))
+                
+                Text("Ease In/Out").tag(Animation.easeInOut(duration: duration).delay(delay))
             }
             .pickerStyle(SegmentedPickerStyle())
-        }
+        }.padding()
         
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
