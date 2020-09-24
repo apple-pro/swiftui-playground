bd3801b7da7ac801f9c320f9baf1d8a08344a197
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-24 18:36:32 +0800

    Prepare demo for view transition: bland, non-animated setup

---
+++ SwiftUIPlayground/Views/TransitionDemo.swift
@@ -0,0 +1,34 @@
+//
+//  TransitionDemo.swift
+//  SwiftUIPlayground
+//
+//  Created by StartupBuilder.INFO on 9/24/20.
+//
+
+import SwiftUI
+
+struct TransitionDemo: View {
+    
+    @State var isHidden = false
+    
+    var body: some View {
+        VStack {
+            Button(isHidden ? "Show" : "Hide") {
+                isHidden.toggle()
+            }
+            
+            if !isHidden {
+                Rectangle()
+                    .fill(Color.orange)
+                    .frame(width: 200, height: 200)
+            }
+            
+        }
+    }
+}
+
+struct TransitionDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionDemo()
+    }
+}
