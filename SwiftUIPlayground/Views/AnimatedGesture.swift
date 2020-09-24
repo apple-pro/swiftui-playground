b0e9a361b664e50a653a38405cd8855c69f003c1
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-24 18:21:06 +0800

    The actual code for moving the view using the drag gesture

--- SwiftUIPlayground/Views/AnimatedGesture.swift
+++ SwiftUIPlayground/Views/AnimatedGesture.swift
@@ -7,7 +7,10 @@
 
 import SwiftUI
 
+//our awesome guide:
+//https://www.hackingwithswift.com/books/ios-swiftui/animating-gestures
 struct AnimatedGesture: View {
+    
     @State private var dragAmount = CGSize.zero
     
     var body: some View {
@@ -15,6 +18,13 @@ struct AnimatedGesture: View {
             .frame(width: 300, height: 200)
             .clipShape(RoundedRectangle(cornerRadius: 10))
             .offset(dragAmount)
+            
+            //this is it for the gesture
+            .gesture(
+                DragGesture()
+                    .onChanged { self.dragAmount = $0.translation }
+                    .onEnded { _ in self.dragAmount = .zero }
+            )
     }
 }
 
