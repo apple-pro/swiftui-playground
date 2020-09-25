f77177d2f50311394a07b0d1b2495e8d076cb968
LM Bibera <lyndon.bibera@headhuntr.io>
2020-09-25 15:28:57 +0800

    Add another option to skip aspect ratio

--- SwiftUIPlayground/Views/ImageDemo.swift
+++ SwiftUIPlayground/Views/ImageDemo.swift
@@ -9,33 +9,46 @@ import SwiftUI
 
 struct ImageDemo: View {
     
-    @State var contentMode: ContentMode = .fit
+    @State var contentMode: Int = 1
     
     var body: some View {
         VStack {
             Spacer()
             
-            Image("moonLarge")
-                .resizable()
-                .aspectRatio(contentMode: contentMode)
-                .frame(width: 300, height: 300)
-                .clipped()
-                .background(Color.red)
-                .padding()
-                .background(Color.blue)
+            if contentMode == 2 {
+                Image("moonLarge")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                    .clipped()
+                    .background(Color.red)
+                    .padding()
+                    .background(Color.blue)
+            } else {
+                Image("moonLarge")
+                    .resizable()
+                    .aspectRatio(contentMode: contentMode == 1 ? .fill : .fit)
+                    .frame(width: 300, height: 300)
+                    .clipped()
+                    .background(Color.red)
+                    .padding()
+                    .background(Color.blue)
+            }
             
             Spacer()
             
             Picker("Gender Identity", selection: $contentMode) {
-                Text("Fit").tag(ContentMode.fit)
-                Text("Fill").tag(ContentMode.fill)
+                Text("Fit").tag(0)
+                Text("Fill").tag(1)
+                Text("None").tag(2)
             }
             .pickerStyle(SegmentedPickerStyle())
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.gray)
+        .background(Color.yellow)
+        .ignoresSafeArea()
     }
+    
 }
 
 struct ImageDemo_Previews: PreviewProvider {
